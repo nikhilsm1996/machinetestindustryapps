c0b04025bf488903b0ffa35fda06b6cf002fcbb7
Nikhil Sam Mani <nikhilsm1996@gmail.com>
2025-03-14 18:56:26 +0530

    homepage

--- frontend/orderfe/src/pages/Homepage.jsx
+++ frontend/orderfe/src/pages/Homepage.jsx
@@ -1,17 +1,13 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 const Home = () => {
-  return (
-    <>
-      <Navbar />
+    return (
       <div className="container text-center mt-5">
         <h1>Welcome to Order Management</h1>
         <p>Manage your orders efficiently and easily.</p>
       </div>
-      <Footer />
-    </>
-  );
-};
+    );
+  };
+  
+  export default Home;
+  
 
-export default Home;
